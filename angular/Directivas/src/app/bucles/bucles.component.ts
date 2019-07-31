06efd558b6e55d05d47526782deb521feb8ae19a
Mario <mariogironm@gmail.com>
2019-07-31 17:03:20 +0200

    Ejemplo ngFor - Episodios GOT

---
+++ angular/Directivas/src/app/bucles/bucles.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bucles',
+  templateUrl: './bucles.component.html',
+  styleUrls: ['./bucles.component.css']
+})
+export class BuclesComponent implements OnInit {
+
+  episodios: any[] = [
+    { title: 'Winter Is Coming', director: 'Tim Van Patten' },
+    { title: 'The Kingsroad', director: 'Tim Van Patten' },
+    { title: 'Lord Snow', director: 'Brian Kirk' },
+    { title: 'Cripples, Bastards, and Broken Things', director: 'Brian Kirk' },
+    { title: 'The Wolf and the Lion', director: 'Brian Kirk' },
+    { title: 'A Golden Crown', director: 'Daniel Minahan' },
+    { title: 'You Win or You Die', director: 'Daniel Minahan' },
+    { title: 'The Pointy End', director: 'Daniel Minahan' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

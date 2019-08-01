1634f5c7e3ca19409d52f8a8bf1c403650d2e0af
Mario <mariogironm@gmail.com>
2019-08-01 13:01:02 +0200

    Ejercicio Reloj - INPUT

--- angular/Tiempo/src/app/app.module.ts
+++ angular/Tiempo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RelojComponent } from './reloj/reloj.component';
+import { RelojSimpleComponent } from './reloj-simple/reloj-simple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelojComponent,
+    RelojSimpleComponent
   ],
   imports: [
     BrowserModule

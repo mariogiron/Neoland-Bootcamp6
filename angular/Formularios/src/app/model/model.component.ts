e8d7719925017c0cc5b355edc4c017a0b23643d2
Mario Giron Martin <mariogironm@gmail.com>
2019-08-07 10:58:30 +0200

    Validaciones Formulario

--- angular/Formularios/src/app/model/model.component.ts
+++ angular/Formularios/src/app/model/model.component.ts
@@ -14,13 +14,20 @@ export class ModelComponent implements OnInit {
     this.formulario = new FormGroup({
       nombre: new FormControl('', [
         Validators.required,
-        Validators.maxLength(10)
+        Validators.maxLength(15)
       ]),
-      apellidos: new FormControl(''),
-      email: new FormControl(''),
+      apellidos: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControl('', [
+        Validators.pattern(/^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/)
+      ]),
       dni: new FormControl(''),
       edad: new FormControl(''),
-      password: new FormControl(''),
+      password: new FormControl('', [
+        Validators.pattern(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/)
+      ]),
       password_repeat: new FormControl('')
     });
   }

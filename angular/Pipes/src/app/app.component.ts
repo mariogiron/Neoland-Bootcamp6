0191e3347f94affdf70da1527aa10dab395e760f
Mario Giron Martin <mariogironm@gmail.com>
2019-08-19 20:22:46 +0200

    Proyecto PIPES

---
+++ angular/Pipes/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  precio: number;
+  fechaActual: Date;
+  numero: number;
+  cadena: string;
+  randomNum: number;
+
+  cadena5Segundos: Promise<string>
+
+  constructor() {
+    this.precio = 4999.99;
+    this.fechaActual = new Date();
+    this.numero = 3489.125789;
+    this.cadena = 'HoLa MUndO';
+    this.randomNum = Math.random();
+
+    this.cadena5Segundos = new Promise((resolve, reject) => {
+      setTimeout(() => resolve('Ha terminado la promesa'), 5000);
+    })
+  }
+
+}

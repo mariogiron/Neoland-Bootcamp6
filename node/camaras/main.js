8bb79b72dcd42815c2930bcca1ff4a0e6a953668
Mario Giron Martin <mariogironm@gmail.com>
2019-08-22 17:42:55 +0200

    Ejemplo VORPAL comandos PREGUNTAS

--- node/camaras/main.js
+++ node/camaras/main.js
@@ -19,3 +19,30 @@ vorpal
     .action((args, callback) => {
         cameras().then(() => callback());
     })
+
+vorpal
+    .command('preguntas', 'Pequeño test con preguntas')
+    .action(function (args, callback) {
+        return this.prompt([
+            {
+                type: 'list',
+                message: 'Selecciona una de las opciones',
+                name: 'opciones',
+                choices: ['primera', 'segunda', 'tercera']
+            },
+            {
+                type: 'confirm',
+                message: '¿De verdad quieres eso?',
+                name: 'confirmacion'
+            },
+            {
+                type: 'input',
+                message: 'Dime tu nombre',
+                name: 'nombre',
+                default: 'Mario Girón'
+            }
+        ], function (results) {
+            console.log(results);
+            callback();
+        });
+    });

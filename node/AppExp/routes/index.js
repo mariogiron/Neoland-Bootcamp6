11163139d2e40188d693a463ae917c3fbe58f164
Mario Giron Martin <mariogironm@gmail.com>
2019-08-23 13:00:08 +0200

    Middlewares Express

--- node/AppExp/routes/index.js
+++ node/AppExp/routes/index.js
@@ -1,10 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
+let login = true;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log('INDEX: ', req.fechaActual);
   res.render('index', { title: 'Express' });
 });
+
+// http://localhost:3000/contacto
+router.get('/contacto', [checkUserToken, checkLogin, middleware1, middleware2], (req, res) => {
+
+  res.send('RESPUESTA FINAL');
+});
+
+router.get('/about', [checkLogin, middleware2], (req, res) => {
+  res.send('RESPUESTA DE ABOUT');
+});
+
+router.route('/libro')
+  .get((req, res) => {
+    res.send('Petición GET sobre /libro')
+  })
+  .post((req, res) => {
+    res.send('Petición POST sobre /libro')
+  })
+  .delete((req, res) => {
+    res.send('Petición DELETE sobre /libro')
+  })
+
+function middleware1(req, res, next) {
+  console.log('Pasa por el primer Middleware');
+  next();
+}
+
+function middleware2(req, res, next) {
+  console.log('Pasa por el segundo Middleware');
+  next();
+}
+
+function checkLogin(req, res, next) {
+  if (login) {
+    next();
+  } else {
+    res.send('Login debe ser igual a true');
+  }
+}
+
+function checkUserToken(req, res, next) {
+  if (req.headers['user-token']) {
+    // Compruebo en BD si el user-token es correcto
+    next();
+  } else {
+    res.send('Debes incluir la cabecera user-token');
+  }
+}
 
 module.exports = router;
--- node/AppExp/routes/users.js
+++ node/AppExp/routes/users.js
@@ -8,9 +8,22 @@ router.get('/', function (req, res, next) {
 });
 
 // http://localhost:3000/users/87
+// http://localhost:3000/users/87?format=json&appid=213213msad
 router.get('/:userId', (req, res) => {
   console.log(req.params.userId);
-  res.send('Manejador de ruta para user con ID: ' + req.params.userId);
+  console.log(req.query.appid);
+  if (req.query.format == 'json') {
+    res.json({ param1: 'value1', param2: 'value2' });
+  } else {
+    res.send('Manejador de ruta para user con ID: ' + req.params.userId);
+  }
+})
+
+//POST http://localhost:3000/users/new
+router.post('/new/:departamento', (req, res) => {
+  console.log(req.body.nombre);
+  console.log(req.params.departamento);
+  res.send('Petición POST sobre /users/new')
 })
 
 module.exports = router;

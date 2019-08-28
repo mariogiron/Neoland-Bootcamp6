135db0d80e610f884bc273acd733d905bbb2c4c4
Mario Giron Martin <mariogironm@gmail.com>
2019-08-28 11:52:59 +0200

    Ejercicio AppEscuela - métodos getAll en modelo alumnos

---
+++ node/AppEscuelas/models/alumnos.js
@@ -0,0 +1,29 @@
+const db = require('../db');
+
+// Recupera todos los alumnos a través de un callback
+const getAll = (done) => {
+    db.get().query('select * from alumnos', (err, rows) => {
+        done(err, rows);
+    })
+}
+
+const getAllP = () => {
+    return new Promise((resolve, reject) => {
+        db.get().query('select * from alumnos', (err, rows) => {
+            if (err) reject(err)
+            resolve(rows)
+        })
+    })
+}
+
+module.exports = {
+    getAll: getAll
+}
+-------
+    modelAlumnos.getAllP()
+        .then((rows) => {
+
+        })
+        .catch((err) => {
+
+        })

267c433abea6c696ea0f0067dde5e1fe4b405177
Mario Giron Martin <mariogironm@gmail.com>
2019-08-30 10:10:16 +0200

    Ejercicio AppEscuela - Api de Estudiantes

--- node/AppEscuelas/routes/api/estudiantes.js
+++ node/AppEscuelas/routes/api/estudiantes.js
@@ -2,18 +2,44 @@ let express = require('express');
 let router = express.Router();
 let alumnoModel = require('../../models/alumnos');
 
-router.use((req, res, next) => {
-    if (req.query.format === 'json') {
-        next();
-    } else {
-        res.json({ amenaza: 'Mi API sólo devuelve JSONSes' })
-    }
-})
+// router.use((req, res, next) => {
+//     if (req.query.format === 'json') {
+//         next();
+//     } else {
+//         res.json({ amenaza: 'Mi API sólo devuelve JSONSes' })
+//     }
+// })
 
+// http://localhost:3000/api/estudiantes
 router.get('/', (req, res) => {
     alumnoModel.getAllP()
         .then(rows => res.json(rows))
         .catch(err => res.json(err))
+});
+
+// http://localhost:3000/api/estudiantes/7
+router.get('/:estudianteId', (req, res) => {
+    alumnoModel.getById(req.params.estudianteId)
+        .then(alumno => res.json(alumno))
+        .catch(err => res.json(err));
+});
+
+router.post('/', (req, res) => {
+    alumnoModel.insert(req.body)
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
+router.put('/', (req, res) => {
+    alumnoModel.update(req.body.id, req.body)
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
+router.delete('/', (req, res) => {
+    alumnoModel.deleteById(req.body.id)
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
 })
 
 module.exports = router;

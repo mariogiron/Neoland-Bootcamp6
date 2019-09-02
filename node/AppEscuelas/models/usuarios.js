e00931cb492b13b532a44341d92a9769419a5948
Mario Giron Martin <mariogironm@gmail.com>
2019-09-02 10:58:39 +0200

    Ejercicio AppEscuela - Token enviado en el login

--- node/AppEscuelas/models/usuarios.js
+++ node/AppEscuelas/models/usuarios.js
@@ -18,7 +18,18 @@ const getById = (pId) => {
     })
 }
 
+const getByUsername = (pUsername) => {
+    return new Promise((resolve, reject) => {
+        db.get().query('select * from usuarios where username = ?', [pUsername], (err, rows) => {
+            if (err) reject(err)
+            if (rows.length != 1) resolve(null)
+            resolve(rows[0])
+        })
+    })
+}
+
 module.exports = {
     insert: insert,
-    getById: getById
+    getById: getById,
+    getByUsername: getByUsername
 }
--- node/AppEscuelas/routes/api/usuarios.js
+++ node/AppEscuelas/routes/api/usuarios.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jwt-simple');
+const moment = require('moment');
 
 let usuariosModel = require('../../models/usuarios');
 
@@ -23,10 +25,47 @@ router.post('/registro', async (req, res) => {
     try {
         let result = await usuariosModel.insert(req.body);
         let usuario = await usuariosModel.getById(result.insertId);
-        res.json(usuario);
+        // res.json(usuario);
+        res.json({ token: createToken(usuario) })
     } catch (err) {
         res.json(err);
     }
-})
+});
+
+router.post('/login', (req, res) => {
+    usuariosModel.getByUsername(req.body.username)
+        .then((user) => {
+            if (user == null) return res.json({ error: 'Usuario y o contraseña erroneos (1)' })
+            bcrypt.compare(req.body.password, user.password, (err, same) => {
+                if (err) return res.json({ error: 'Error!!!!' })
+                if (!same) return res.json({ error: 'Usuario y o contraseña erroneos (2)' })
+                res.json({ token: createToken(user) })
+            });
+        })
+        .catch((err) => {
+            res.json(err);
+        })
+});
+
+router.post('/loginv2', async (req, res) => {
+    try {
+        let user = await usuariosModel.getByUsername(req.body.username);
+        if (user == null) return res.json({ error: 'Usuario y o contraseña erroneos (1)' });
+        let same = bcrypt.compareSync(req.body.password, user.password);
+        if (!same) return res.json({ error: 'Usuario y o contraseña erroneos (2)' });
+        res.json({ success: 'Usuario correcto' });
+    } catch (err) {
+        res.json({ error: err })
+    }
+});
+
+const createToken = (pUser) => {
+    const payload = {
+        userId: pUser.id,
+        createdAt: moment().unix(),
+        expiresAt: moment().add(5, 'minutes').unix()
+    }
+    return jwt.encode(payload, process.env.SECRET_KEY);
+}
 
 module.exports = router;

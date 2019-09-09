6901b34c9e6e266906f839a404397111c74230d2
Mario Giron Martin <mariogironm@gmail.com>
2019-09-09 11:34:28 +0200

    Ejercicio BOT - código base

---
+++ node/chatbot/index.js
@@ -0,0 +1,33 @@
+// Instalar las librerías
+// Variable de entorno
+// NGROK
+require('dotenv').config();
+
+const Telegraf = require('telegraf')
+const express = require('express')
+const expressApp = express()
+
+const testCommand = require('./commands/test');
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+expressApp.use(bot.webhookCallback('/secret-path'))
+bot.telegram.setWebhook('https://8d07598b.ngrok.io/secret-path')
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+expressApp.post('/secret-path', (req, res) => {
+    res.send('Petición POST del webhook');
+})
+
+bot.command('start', (ctx) => {
+    console.log(ctx.message);
+    ctx.reply('Bienvenido al BOT. /test Saludos');
+})
+
+bot.command('test', testCommand);
+
+expressApp.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})

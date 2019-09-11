3d686596b33fe892f57c1d3eb726b85cbd92b34f
Mario Giron Martin <mariogironm@gmail.com>
2019-09-11 10:27:55 +0200

    Ionic - Página planetas creada

--- ionic/MiAppMovil/src/app/app-routing.module.ts
+++ ionic/MiAppMovil/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'planetas', loadChildren: './planetas/planetas.module#PlanetasPageModule' }
 ];
 
 @NgModule({
---
+++ ionic/MiAppMovil/src/app/planetas/planetas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planetas',
+  templateUrl: './planetas.page.html',
+  styleUrls: ['./planetas.page.scss'],
+})
+export class PlanetasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
